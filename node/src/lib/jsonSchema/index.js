4b328804d3169e63b4b1f437a3b79c7aec2a2a30
Rolf Streefkerk <r.streefkerk@gmail.com>
2020-01-26 15:52:11 +0700

    added json schema validation

---
+++ node/src/lib/jsonSchema/index.js
@@ -0,0 +1,37 @@
+const Ajv = require('ajv')
+const ajv = new Ajv({ schemaId: 'id' }) // we using version 4
+ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'))
+
+/**
+ *
+ * Instantiates the JSON (or part) object as a JSON Schema
+ * and compiles it with Ajv
+ *
+ * @param {object} schema
+ */
+function JsonSchema (schema) {
+  this.schema = schema
+  this.validate = ajv.compile(this.schema)
+}
+
+/**
+ * Validate against the schema
+ *
+ * @param {object} input
+ * @returns {object} resp
+ */
+JsonSchema.prototype.validateSchema = function (input) {
+  const valid = this.validate(input)
+  const resp = {
+    valid: false,
+    errors: {}
+  }
+
+  if (!valid) resp.errors = this.validate.errors
+  else resp.valid = true
+
+  return resp
+}
+
+// export the class
+module.exports = JsonSchema
